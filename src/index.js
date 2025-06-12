45f0f09eb5459aec2620bfce442189a51ac0d06f
naveeng80 <naveen80.g@gmail.com>
2025-06-12 17:19:00 +0100

    code edited

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello World! Prabhakar')
+	res.send('Hello World! Prabhakar ingress')
 })
 
 app.listen(80, () => console.log('App running on port 80'))
